85f7e8fad543c8adb39d48621b2b4a1cb2dc0722
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-01-14 15:37:44 +0100

    First Setup + data

---
+++ js/script.js
@@ -0,0 +1,31 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Vue Slider',
+        images: [
+            {
+                name: '1',
+                imageLink: 'https://unsplash.it/600/300?image=34'
+            },
+            {
+                name: '2',
+                imageLink: 'https://unsplash.it/600/300?image=90'
+                },
+                {
+                    ame: '3',
+                imageLink: 'https://unsplash.it/600/300?image=123'
+                },
+                {
+                name: '4',
+                imageLink: 'https://unsplash.it/600/300?image=78'
+                },
+                {
+                name: '5',
+                imageLink: 'https://unsplash.it/600/300?image=795'
+                }
+        ] 
+    },
+    methods: {
+
+    }
+});
